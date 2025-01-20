fc0ec051ace816092569e641e8574283ac042e34
Giovani da Silva Ramos <gi_1997@hotmail.com>
2025-01-20 02:31:18 -0300

    Local Storage

--- javascript/script.js
+++ javascript/script.js
@@ -12,8 +12,7 @@ const filterBtn = document.querySelector("#filter-select");
 let oldInputValue;
 
 //Funções
-const saveTodo = (text) => {
-
+const saveTodo = (text, done = 0, save = 1) => {
     const todo = document.createElement("div");
     todo.classList.add("todo");
 
@@ -35,6 +34,15 @@ const saveTodo = (text) => {
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     todo.appendChild(deleteBtn);
+
+    // Utilizando dados da LocalStorage
+    if(done) {
+        todo.classList.add("done")
+    }
+
+    if(save) {
+        saveTodoLocalStorage({text, done})
+    }
 
     todoList.appendChild(todo);
 
@@ -180,3 +188,19 @@ filterBtn.addEventListener("change", (e) => {
     filterTodos(filterValue);
 
 })
+
+//Local Storage
+
+const getTodosLocalStorage = () => {
+    const todos = JSON.parse(localStorage.getItem("todos")) || []
+
+    return todos;
+}
+
+const saveTodoLocalStorage = (todo) => {
+    const todos = getTodosLocalStorage()
+    
+    todos.push(todo)
+
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
